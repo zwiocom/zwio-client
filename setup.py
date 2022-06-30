1eeb6fea3316c22f838ea2f5d0077f6d12ee80c4
Daniel Humphreys <dan_humphreys@yahoo.co.uk>
2022-06-30 19:51:30 +0100

    Added setup.py.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='zwio-client',
     version='0.1.0',
     packages=['src', 'test'],
-    url='www.zwio.com',
+    url='https://www.zwio.com',
     license='MIT',
     author='Daniel Humphreys',
     author_email='dan_humphreys@yahoo.co.uk',
